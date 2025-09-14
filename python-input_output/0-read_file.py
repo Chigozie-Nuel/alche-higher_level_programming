b9366a8c31877a8bc612bb92087af9f00a902882
Ndubuaku Chigozie <c.ndubuaku@alustudent.com>
2025-09-14 02:05:17 -0700

    Updated the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a function to read and print the contents of a UTF-8 text file."""
+"""Module that defines a function to read and print a UTF-8 text file to stdout."""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, encoding="utf-8") as f:
